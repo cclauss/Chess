0b8e8f9b535d9e0ac7808acb5de95d335f6350e7
671620616 <blerghhh86@gmail.com>
2015-03-07 17:07:29 -0500

    Improved installer
    Made installer more betterer

--- Phantom_installer.py
+++ Phantom_installer.py
@@ -2,18 +2,20 @@
 """
 Run this file to download PhantomChess to the directory of this file.
 """
-
+print "Preparing to install..."
 import zipfile
 import os
 import shutil
 import urllib
-
+print "Downloading..."
 url = 'https://github.com/671620616/PhantomChess/archive/master.zip'
 urllib.urlretrieve(url, 'Phantom.zip')
+print "Unzipping..."
 zipped = zipfile.ZipFile('Phantom.zip', 'r')
 zipped.extractall()
 zipped.close()
-
+print "Cleaing up..."
 shutil.copytree(os.path.join('PhantomChess-master', 'Phantom'), 'Phantom')
 shutil.rmtree('PhantomChess-master')
 os.remove('Phantom.zip')
+print "Done!"
