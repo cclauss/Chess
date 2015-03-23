c13ee331044cc78d28490c9f4365f503ece4a45d
cclauss <cclauss@bluewin.ch>
2015-03-23 02:49:14 +0100

    Use x, y to optimize by removing the dots

--- Phantom/core/coord/vectored_lists.py
+++ Phantom/core/coord/vectored_lists.py
@@ -31,46 +31,54 @@ __all__ = []
 @call_trace(9)
 @integer_args
 def north(piece):
-    return [Coord(piece.coord.x, i) for i in xrange(piece.coord.y+1, grid_height)]
+    x, y = piece.coord.as_tup()
+    return [Coord(x, i) for i in xrange(y+1, grid_height)]
 
 @call_trace(9)
 @integer_args
 def south(piece):
-    return [Coord(piece.coord.x, i) for i in xrange(piece.coord.y-1, -1, -1)]
+    x, y = piece.coord.as_tup()
+    return [Coord(x, i) for i in xrange(y-1, -1, -1)]
 
 @call_trace(9)
 @integer_args
 def east(piece):
-    return [Coord(i, piece.coord.y) for i in xrange(piece.coord.x+1, grid_width)]
+    x, y = piece.coord.as_tup()
+    return [Coord(i, y) for i in xrange(x+1, grid_width)]
 
 @call_trace(9)
 @integer_args
 def west(piece):
-    return [Coord(i, piece.coord.y) for i in xrange(piece.coord.x-1, -1, -1)]
+    x, y = piece.coord.as_tup()
+    return [Coord(i, y) for i in xrange(x-1, -1, -1)]
     
 @call_trace(9)
 @integer_args
 def ne(piece):
-    iterto = min(grid_width-piece.coord.x, grid_height-piece.coord.y)
-    return [Coord(piece.coord.x+i, piece.coord.y+i) for i in xrange(1, iterto)]
+    x, y = piece.coord.as_tup()
+    iterto = min(grid_width-x, grid_height-y)
+    return [Coord(x+i, y+i) for i in xrange(1, iterto)]
 
 @call_trace(9)
 @integer_args
 def se(piece):
-    iterto = min(grid_width-piece.coord.x, piece.coord.y)
-    return [Coord(piece.coord.x+i, piece.coord.y-i) for i in xrange(1, iterto+1)]
+    x, y = piece.coord.as_tup()
+    iterto = min(grid_width-x, y)
+    return [Coord(x+i, y-i) for i in xrange(1, iterto+1)]
 
 @call_trace(9)
 @integer_args
 def nw(piece):
-    iterto = min(piece.coord.x, grid_height-piece.coord.y)
-    return [Coord(piece.coord.x-i, piece.coord.y+i) for i in xrange(1, iterto+1)]
+    x, y = piece.coord.as_tup()
+    iterto = min(x, grid_height-y)
+    return [Coord(x-i, y+i) for i in xrange(1, iterto+1)]
 
 @call_trace(9)
 @integer_args
 def sw(piece):
-    iterto = min(piece.coord.x, piece.coord.y)
-    return [Coord(piece.coord.x-i, piece.coord.y-i) for i in xrange(1, iterto+1)]
+    x, y = piece.coord.as_tup()
+    iterto = min(x, y)
+    return [Coord(x-i, y-i) for i in xrange(1, iterto+1)]
 
 @call_trace(9)
 @integer_args
